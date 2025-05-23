f47d4f0f07dac27df732702d44de0e51dd255b0c
gentlsnek <erwinpimenta1644@gmail.com>
2025-05-23 21:36:31 +0530

    bruh shiz

--- functions/Deal.go
+++ functions/Deal.go
@@ -1,7 +1,6 @@
 package functions
 
 func Deal(deck []string, numcards int) ([]string, []string) {
-	// Deal the specified number of cards from the deck
 	if numcards > len(deck) {
 		numcards = len(deck)
 	}
